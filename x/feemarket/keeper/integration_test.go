b2b79472927ad254ece593f1abdb2e36314eb087
Vlad J <vladjdk@gmail.com>
2025-05-30 17:48:49 -0400

    (chore): move feemarket update BaseFee to EndBlock (#190)
    * move feemarket update to endblock
    
    * adjust comments

--- x/feemarket/keeper/integration_test.go
+++ x/feemarket/keeper/integration_test.go
@@ -397,16 +397,21 @@ var _ = Describe("Feemarket", func() {
 						bz, err := s.factory.EncodeTx(tx)
 						Expect(err).To(BeNil())
 
+						err = s.network.NextBlock()
+						Expect(err).To(BeNil())
+
 						res, err := s.network.CheckTx(bz)
 						Expect(err).To(BeNil())
 						Expect(res.IsOK()).To(BeFalse())
 						Expect(res.Log).To(ContainSubstring("insufficient fee"))
 					},
 					Entry("legacy tx", func() txParams {
-						return txParams{big.NewInt(baseFee - 2_000_000_000), nil, nil, nil}
+						currBaseFee := s.network.App.FeeMarketKeeper.GetBaseFee(s.network.GetContext())
+						return txParams{big.NewInt(currBaseFee.Sub(currBaseFee.Sub(math.LegacyNewDec(minGasPrices)).QuoInt64(2)).RoundInt64()), nil, nil, nil}
 					}),
 					Entry("dynamic tx", func() txParams {
-						return txParams{nil, big.NewInt(baseFee - 2_000_000_000), big.NewInt(0), &ethtypes.AccessList{}}
+						currBaseFee := s.network.App.FeeMarketKeeper.GetBaseFee(s.network.GetContext())
+						return txParams{nil, big.NewInt(currBaseFee.Sub(currBaseFee.Sub(math.LegacyNewDec(minGasPrices)).QuoInt64(2)).RoundInt64()), big.NewInt(0), &ethtypes.AccessList{}}
 					}),
 				)
 
