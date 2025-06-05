a8ab4a1abf429478ab3ef77d8f8f2ab446bc71de
zsystm <actor93kor@gmail.com>
2025-06-05 13:34:42 +0900

    fix ci issue

--- ante/cosmos/authz_test.go
+++ ante/cosmos/authz_test.go
@@ -24,6 +24,7 @@ func TestAuthzLimiterDecorator(t *testing.T) {
 	evmConfigurator := evmtypes.NewEVMConfigurator().
 		WithEVMCoinInfo(constants.ExampleChainCoinInfo[constants.ExampleChainID])
 	err := evmConfigurator.Configure()
+	require.NoError(t, err)
 
 	encodingCfg := encoding.MakeConfig(constants.ExampleChainID.EVMChainID)
 	txCfg := encodingCfg.TxConfig
