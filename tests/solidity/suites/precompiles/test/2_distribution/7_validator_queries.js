05667efe515fc22d13a37d46549c3e6d0fe3f864
zsystm <actor93kor@gmail.com>
2025-07-08 11:00:37 +0900

    add validator queries tc

---
+++ tests/solidity/suites/precompiles/test/2_distribution/7_validator_queries.js
@@ -0,0 +1,50 @@
+const { expect } = require('chai');
+const hre = require('hardhat');
+
+/**
+ * Hardhat tests for Distribution precompile validator query methods.
+ * Covers validatorDistributionInfo, validatorSlashes and delegatorValidators.
+ */
+describe('Distribution – validator query methods', function () {
+    const DIST_ADDRESS = '0x0000000000000000000000000000000000000801';
+    const VAL_OPER_BECH32 = 'cosmosvaloper1cml96vmptgw99syqrrz8az79xer2pcgpqqyk2g';
+    const VAL_BECH32 = 'cosmos1cml96vmptgw99syqrrz8az79xer2pcgp95srxm'
+
+    let distribution, signer;
+
+    before(async () => {
+        [signer] = await hre.ethers.getSigners();
+        distribution = await hre.ethers.getContractAt('DistributionI', DIST_ADDRESS);
+    });
+
+    it('validatorDistributionInfo returns current distribution info', async function () {
+        const info = await distribution.validatorDistributionInfo(VAL_OPER_BECH32);
+        console.log('validatorDistributionInfo:', info);
+        expect(info.operatorAddress).to.equal(VAL_BECH32);
+        expect(info.selfBondRewards).to.be.an('array');
+        expect(info.commission).to.be.an('array');
+    });
+
+    it('validatorSlashes returns slashing events (none expected)', async function () {
+        const pageReq = { key: '0x', offset: 0, limit: 100, countTotal: true, reverse: false };
+        const [slashes, pageResponse] = await distribution.validatorSlashes(
+            VAL_OPER_BECH32,
+            1,
+            5,
+            pageReq
+        );
+        console.log('validatorSlashes:', slashes, pageResponse);
+        expect(slashes).to.be.an('array');
+        expect(slashes.length).to.equal(Number(pageResponse.total.toString()));
+    });
+
+    it('delegatorValidators lists validators for delegator', async function () {
+        const amountBn = hre.ethers.parseEther('0.001');
+        const amount = BigInt(amountBn.toString());
+
+        const validators = await distribution.delegatorValidators(signer.address);
+        console.log('delegatorValidators:', validators);
+        console.log(validators)
+        expect(validators).to.include(VAL_OPER_BECH32);
+    });
+});
