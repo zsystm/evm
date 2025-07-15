058eb6d928f43b5826d2ed5ca2b29db5b5d666b6
mmsqe <tqd0800210105@gmail.com>
2025-07-15 22:54:14 +0800

    test: cleanup EvmAppOptions related config (#263)
    * abci
    
    * eip1559
    
    * grpc
    
    * msg
    
    * params
    
    * keeper
    
    * integration
    
    * rename
    
    * align utils test that depends on NewTestSuite
    
    bank
    
    config
    
    evm
    
    * keep evmd -> evm dependency
    
    * set antehandler before seal
    
    * cleanup
    
    * cleanup cfg
    
    * cleanup
    
    * cleanup
    
    * reuse
    
    * vm
    
    * genesis
    
    * fee
    
    * param
    
    * statedb
    
    * add evmd
    
    * revert
    
    * test: cleanup EvmAppOptions related config

--- evmd/app.go
+++ evmd/app.go
@@ -18,9 +18,9 @@ import (
 	dbm "github.com/cosmos/cosmos-db"
 	evmante "github.com/cosmos/evm/ante"
 	cosmosevmante "github.com/cosmos/evm/ante/evm"
+	evmconfig "github.com/cosmos/evm/config"
 	evmosencoding "github.com/cosmos/evm/encoding"
 	"github.com/cosmos/evm/evmd/ante"
-	evmdconfig "github.com/cosmos/evm/evmd/cmd/evmd/config"
 	srvflags "github.com/cosmos/evm/server/flags"
 	cosmosevmtypes "github.com/cosmos/evm/types"
 	"github.com/cosmos/evm/x/erc20"
@@ -32,6 +32,7 @@ import (
 	feemarkettypes "github.com/cosmos/evm/x/feemarket/types"
 	ibccallbackskeeper "github.com/cosmos/evm/x/ibc/callbacks/keeper"
 	// NOTE: override ICS20 keeper to support IBC transfers of ERC20 tokens
+	evmdconfig "github.com/cosmos/evm/evmd/cmd/evmd/config"
 	"github.com/cosmos/evm/x/ibc/transfer"
 	transferkeeper "github.com/cosmos/evm/x/ibc/transfer/keeper"
 	transferv2 "github.com/cosmos/evm/x/ibc/transfer/v2"
@@ -210,7 +211,7 @@ func NewExampleApp(
 	loadLatest bool,
 	appOpts servertypes.AppOptions,
 	evmChainID uint64,
-	evmAppOptions evmdconfig.EVMOptionsFn,
+	evmAppOptions evmconfig.EVMOptionsFn,
 	baseAppOptions ...func(*baseapp.BaseApp),
 ) *EVMD {
 	encodingConfig := evmosencoding.MakeConfig(evmChainID)
