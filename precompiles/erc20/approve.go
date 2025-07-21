17c65a70c7169f119c9a81f5e98ebb1d4ccb9067
Hyunwoo Lee <124245155+zsystm@users.noreply.github.com>
2025-07-22 02:56:17 +0900

    should use owner, not contract address itself (#317)
    Co-authored-by: Alex | Interchain Labs <alex@interchainlabs.io>

--- precompiles/erc20/approve.go
+++ precompiles/erc20/approve.go
@@ -66,8 +66,7 @@ func (p Precompile) Approve(
 		return nil, err
 	}
 
-	// TODO: check owner?
-	if err := p.EmitApprovalEvent(ctx, stateDB, p.Address(), spender, amount); err != nil {
+	if err := p.EmitApprovalEvent(ctx, stateDB, owner, spender, amount); err != nil {
 		return nil, err
 	}
 
